e6a8a5184a62d53c042542474bc207b7f63d2ffc
tjdgus3160 <tjdgus3160@gmail.com>
2021-01-03 23:24:47 +0900

    MockMvc 테스트

---
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/controller/HelloWorldControllerTest.java
@@ -0,0 +1,42 @@
+package com.fastcampus.javaallinone.project3.mycontact.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    private MockMvc mockMvc;
+
+    @Test
+    void helloWorld(){
+//        System.out.println("test");
+        System.out.println(helloWorldController.helloWorld());
+
+        assertEquals("HelloWorld",helloWorldController.helloWorld());
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/helloWorld"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("HelloWorld"));
+    }
+
+}
